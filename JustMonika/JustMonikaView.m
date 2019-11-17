70bee67583028cf77ee6014bf216eb3d6e122b3e
ilammy <me@ilammy.net>
2019-11-17 22:31:14 +0200

    Display static Monika
    Add original Monika background image into assets. Alternatively we could
    have just placed it there manually, but let's use 'proper' means. I want
    to study the Apple way of doing things.
    
    Override awakeFromNib to setup the view in the preview application. This
    is like usual for interfaces created in Interface Builder.
    
    It turns out that it's not enough to just slap an IB_DESIGNABLE onto the
    view. While its drawing mechanisms are called as expected, the view is
    initalized differently. There is a special method that we need to call.
    
    With this the view behaves identically in preview app, in designed UI in
    Interface Builder, in screen saver preview, and as a screen saver. It's
    even 'functional', in a way.

--- JustMonika/JustMonikaView.m
+++ JustMonika/JustMonikaView.m
@@ -10,13 +10,42 @@
 
 @implementation JustMonikaView
 
+// Called by JustMonikaPreview.app
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self setupCALayer];
+}
+
+// Called by Interface Builder preview
+- (void)prepareForInterfaceBuilder
+{
+    [self setupCALayer];
+}
+
+// Called by Screen Saver framework
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
         [self setAnimationTimeInterval:1/30.0];
+        [self setupCALayer];
     }
     return self;
+}
+
+- (void)setupCALayer
+{
+    // Screen savers are loaded as plugins so their main bundle is not
+    // this one, but the host bundle. We need to use the name directly.
+    NSBundle *bundle = [NSBundle bundleWithIdentifier:@"net.ilammy.JustMonika"];
+    NSImage *image = [bundle imageForResource:@"monika_bg"];
+
+    CALayer *layer = [CALayer new];
+    layer.backgroundColor = [[NSColor blackColor] CGColor];
+    layer.contents = image;
+    layer.contentsGravity = kCAGravityResizeAspectFill;
+    self.layer = layer;
 }
 
 - (void)startAnimation
