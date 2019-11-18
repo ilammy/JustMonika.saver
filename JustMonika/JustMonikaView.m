bd08f248720c7c9ebc439dfc4f2fd64f69485e19
ilammy <me@ilammy.net>
2019-11-18 22:10:20 +0200

    Introduce sublayers
    We'll need to have quite a few moving parts here so we'll organize
    them into sublayers. For example, the highlight is implemented by
    a similar bitmap with varying opacity. We don't have opacity animation
    now, but here's a bitmap.
    
    Sublayers seem to be tricky. For now I've hacked their positioning
    via layouts, but I think there could be a better way. To use constraints
    we need to explicitly import the QuartzCore framework which we have been
    using implicitly via ScreenSaver import up until this point.

--- JustMonika/JustMonikaView.m
+++ JustMonika/JustMonikaView.m
@@ -8,6 +8,15 @@
 
 #import "JustMonikaView.h"
 
+#import <QuartzCore/CoreAnimation.h>
+
+@interface JustMonikaView ()
+
+@property(nonatomic) CALayer *sceneLayer;
+@property(nonatomic) CALayer *lightLayer;
+
+@end
+
 @implementation JustMonikaView
 
 // Called by JustMonikaPreview.app
@@ -39,13 +48,49 @@
     // Screen savers are loaded as plugins so their main bundle is not
     // this one, but the host bundle. We need to use the name directly.
     NSBundle *bundle = [NSBundle bundleWithIdentifier:@"net.ilammy.JustMonika"];
-    NSImage *image = [bundle imageForResource:@"monika_bg"];
+    NSImage *scene = [bundle imageForResource:@"monika_bg"];
+    NSImage *light = [bundle imageForResource:@"monika_bg_highlight"];
+
+    self.sceneLayer = centeredSublayerWithImage(scene);
+    self.lightLayer = centeredSublayerWithImage(light);
 
     CALayer *layer = [CALayer new];
     layer.backgroundColor = [[NSColor blackColor] CGColor];
-    layer.contents = image;
     layer.contentsGravity = kCAGravityResizeAspectFill;
+    layer.layoutManager = [CAConstraintLayoutManager layoutManager];
+
+    [layer addSublayer:self.sceneLayer];
+    [layer addSublayer:self.lightLayer];
+
     self.layer = layer;
+}
+
+static CAConstraint *centerX;
+static CAConstraint *centerY;
+static dispatch_once_t constraintToken;
+
+static CALayer* centeredSublayerWithImage(NSImage *image)
+{
+    CALayer *layer = [CALayer new];
+    layer.contents = image;
+
+    // Bounds need to be set explicitly for sublayers
+    layer.bounds = CGRectMake(0, 0, image.size.width, image.size.height);
+
+    // Constraints to ensure that sublayer is centered within its superlayer
+    dispatch_once(&constraintToken, ^{
+        centerX = [CAConstraint constraintWithAttribute:kCAConstraintMidY
+                                             relativeTo:@"superlayer"
+                                              attribute:kCAConstraintMidY];
+        centerY = [CAConstraint constraintWithAttribute:kCAConstraintMidX
+                                             relativeTo:@"superlayer"
+                                              attribute:kCAConstraintMidX];
+    });
+
+    [layer addConstraint:centerX];
+    [layer addConstraint:centerY];
+
+    return layer;
 }
 
 - (void)startAnimation
