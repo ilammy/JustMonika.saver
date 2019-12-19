a3f75d7e2eb8d0b145c47de14422a5371b72eeb0
ilammy <me@ilammy.net>
2019-12-20 00:45:50 +0200

    bicubic filtering?
    https://stackoverflow.com/questions/13501081/efficient-bicubic-filtering-code-in-glsl

--- JustMonikaGL/res/Shaders/fragment.glsl
+++ JustMonikaGL/res/Shaders/fragment.glsl
@@ -16,19 +16,61 @@ uniform float offsetY;
 // All our textures have 2048 x 1048 size in memory and have 1280 x 720
 // starting at origin filled with actually userful data. UV coordinates
 // are expressed in source image coordinates.
-const vec2 textureSize = vec2(2048.0, 1024.0);
+const vec2 textureSize2 = vec2(2048.0, 1024.0);
 const vec2 imageSize   = vec2(1280.0,  720.0);
+
+vec4 cubic(float v) {
+    vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
+    vec4 s = n * n * n;
+    float x = s.x;
+    float y = s.y - 4.0 * s.x;
+    float z = s.z - 4.0 * s.y + 6.0 * s.x;
+    float w = 6.0 - x - y - z;
+    return vec4(x, y, z, w) * (1.0/6.0);
+}
+
+vec4 textureBicubic(sampler2D sampler, vec2 texCoords) {
+    vec2 texSize = textureSize(sampler, 0);
+    vec2 invTexSize = 1.0 / texSize;
+
+    texCoords = texCoords * texSize - 0.5;
+
+    vec2 fxy = fract(texCoords);
+    texCoords -= fxy;
+
+    vec4 xcubic = cubic(fxy.x);
+    vec4 ycubic = cubic(fxy.y);
+
+    vec4 c = texCoords.xxyy + vec2(-0.5, +1.5).xyxy;
+
+    vec4 s = vec4(xcubic.xz + xcubic.yw, ycubic.xz + ycubic.yw);
+    vec4 offset = c + vec4 (xcubic.yw, ycubic.yw) / s;
+
+    offset *= invTexSize.xxyy;
+
+    vec4 sample0 = texture(sampler, offset.xz);
+    vec4 sample1 = texture(sampler, offset.yz);
+    vec4 sample2 = texture(sampler, offset.xw);
+    vec4 sample3 = texture(sampler, offset.yw);
+
+    float sx = s.x / (s.x + s.y);
+    float sy = s.z / (s.z + s.w);
+
+    return mix(mix(sample3, sample2, sx),
+               mix(sample1, sample0, sx),
+               sy);
+}
 
 vec4 getPixel(in sampler2D sampler, in vec2 uv)
 {
     const mat4 textureTransform = mat4(
-        1.0/textureSize.x, 0.0, 0.0, 0.0,
-        0.0, 1.0/textureSize.y, 0.0, 0.0,
+        1.0/textureSize2.x, 0.0, 0.0, 0.0,
+        0.0, 1.0/textureSize2.y, 0.0, 0.0,
         0.0,               0.0, 1.0, 0.0,
         0.0,               0.0, 0.0, 1.0
     );
     vec4 spatial = vec4(mod(uv, imageSize), 0.0, 1.0);
-    return texture(sampler, vec2(textureTransform * spatial));
+    return textureBicubic(sampler, vec2(textureTransform * spatial));
 }
 
 void draw(inout vec4 canvas, in vec4 color)
