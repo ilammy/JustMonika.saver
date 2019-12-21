cca88082492d53341ef2b4f7c576e16172500c66
ilammy <me@ilammy.net>
2019-12-21 15:51:53 +0200

    Disable mipmaps and anisotropic filtering
    We do not really need anisotropic filtering because we're looking
    straight at the texture. It produces bleeding artifacts without
    actually improving anything. Drop it.
    
    Disable mipmaps for a moment too and use pure bilinear filtering.
    Now we have removed (almost) all bleeding. However, minimized image
    looks very jaggy because mipmaps are disabled.

--- JustMonikaGL/src/texture.c
+++ JustMonikaGL/src/texture.c
@@ -194,7 +194,6 @@ static GLuint load_png_texture(png_structp png, png_infop png_info)
 {
     GLuint texture = 0;
     struct texture_buffer buffer;
-    float max_anisotropy = 0.0;
 
     memset(&buffer, 0, sizeof(buffer));
 
@@ -246,11 +245,8 @@ static GLuint load_png_texture(png_structp png, png_infop png_info)
                  GL_RGBA,           /* data format */
                  GL_UNSIGNED_BYTE,  /* data type */
                  buffer.data);
-    glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max_anisotropy);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_anisotropy);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
 error:
     destroy_texture_buffer(png, png_info, &buffer);
